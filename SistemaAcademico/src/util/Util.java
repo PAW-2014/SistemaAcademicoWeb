24c699c7ef5eaaeefc99ed97bdccd058e802ab76
TioNoob <arthur.lyrio.oliveira@gmail.com>
2014-05-10 20:50:39 -0300

    Created the negative methods and protected Constructor.

--- SistemaAcademico/src/util/Util.java
+++ SistemaAcademico/src/util/Util.java
@@ -4,8 +4,22 @@ import java.util.Collection;
 
 public class Util {
 	
+	/**
+	 * Because it's a static class
+	 */
+	protected Util(){
+	}
+	
+	public static boolean isNotValid(Collection<? extends Object> list){
+		return !isValid(list);
+	}
+	
 	public static boolean isValid(Collection<? extends Object> list){
 		return (!list.isEmpty());
+	}
+	
+	public static boolean isNotValid(String string){
+		return !isValid(string);
 	}
 
 	public static boolean isValid(String string){
