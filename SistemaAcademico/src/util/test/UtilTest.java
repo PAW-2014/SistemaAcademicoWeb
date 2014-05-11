ebffcc96a803d27dc895bc6e9e47fe55ab9a6fa4
TioNoob <arthur.lyrio.oliveira@gmail.com>
2014-05-11 04:45:13 -0300

    Creation of Util's test.

---
+++ SistemaAcademico/src/util/test/UtilTest.java
@@ -0,0 +1,34 @@
+package util.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UtilTest { // TODO Tests
+
+	@Test
+	public void testStartsWithSpace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsNotValidCollectionOfQextendsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidCollectionOfQextendsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsNotValidObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidObject() {
+		fail("Not yet implemented");
+	}
+
+}
